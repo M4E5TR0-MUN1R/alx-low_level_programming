912749184ae1fd6b1cf32d20c897397933f5188e
M4E5TR0-MUN1R <khamistsanuo@gmail.com>
2023-02-22 00:35:40 +0300

    Task 2: 10x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * print_alphabet - A function to print the alphabet.
+  *
+  * Return: Alway 0 Success
+  *
+  */
+void print_alphabet(void)
+{
+	int i, j;
+
+	for (j = 0; j <= 10; j++)
+	{
+		for (i = 97; i <= 122; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}

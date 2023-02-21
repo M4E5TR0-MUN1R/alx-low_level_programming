f1ac66b50f49a0d01d51810d33c4569109bc7eaf
M4E5TR0-MUN1R <khamistsanuo@gmail.com>
2023-02-21 21:44:45 +0300

    Task 0: _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * main - Program that prints _putchar
+  *
+  * Return: Always (0) Success
+  *
+  */
+int main(void)
+{
+	_putchar();
+	return (0);
+}

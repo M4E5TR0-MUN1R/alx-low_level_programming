38eb90c475cacc95c835d04578f2a1a06ec4583e
M4E5TR0-MUN1R <khamistsanuo@gmail.com>
2023-02-28 00:24:04 +0300

    Task 0: Betty Correction

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 /**
-  * reset_to:98 - A function that sets the arguement value to 98
-  *
+  * reset_to_98 - A function that sets the arguement value to 98
+  * @n: A pointer to the value to be changed
   * Return: NULL
   */
 

53d69c1acaba969cb7db9f9f5eac63b98e6b9377
M4E5TR0-MUN1R <khamistsanuo@gmail.com>
2023-02-28 00:43:56 +0300

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+/**
+  * _strlen - A function that returns the length of a string
+  * @s:  A pointer to the first element of the string
+  * Return: Integer (number of elements)
+  */
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	do
+	{
+		len++;
+	}
+	while(*(s+len) != 0);
+	return (len);
+}

5a0468e7c70eab1943e192a445bdf8d14cb3fc45
M4E5TR0-MUN1R <khamistsanuo@gmail.com>
2023-03-03 11:28:00 +0300

    Finally Cracked with C

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,64 +2,99 @@
 #include <stdlib.h>
 #include <time.h>
 
-/**
-  *
-  *
-  *
-  *
-  */
-int checkHexSum(char *s);
+int checkHexSum(char *k);
+void modify(int t, char *k);
 
+/**
+  * main - This is a program to generate a crack for the program 101-crackme
+  * Return: Always 0 Success
+  */
 int main(void)
 {
-	int i = 0, j = 0, sum = 0;
-	char s[62]="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-	char key[]="";
 	srand(time(NULL));
+	char s[]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9'};
+	int random = 0;
+	int count = 0;
+	int clear = 0;
+	char k[100] = "";
 
-	while(sum != 2772)
+	while (checkHexSum(k) != 2772)
 	{
-		int random =  rand() % 61;
-		printf("%d \n", random);
-		*(key + j) = *(s + random);
-		printf("Key is: %s \n", key);
-		/*
-		if (checkHexSum(key) == 2772)
+		char temp;
+
+		random = (rand() % 62);
+		temp = s[random];
+		*(k + count) = temp;
+		if (checkHexSum(k) > 2772)
 		{
 			break;
 		}
-		else if(checkHexSum(key) > 2772)
+		else if (checkHexSum(k) == 2772)
 		{
-			int c = 0, i = 0;
-			while (*(key + c) != '\0')
-			{
-				c++;
-			}
-			c--;
-			for(i=0; i<c ;i++)
-			{
-				*(key + i) = 0;
-			}
+			break;
 		}
-		*/
-		j++;
+		count++;
 	}
-	sum = checkHexSum(key);
-	printf("Found it: %d \n", sum);
-	printf("Key is: %s \n", key);
+	/*
+	 * printf("k is: %s \n", k);
+	 * printf("The sum of ASCII Codes: %d \n", checkHexSum(k));
+	 */
+	if (checkHexSum(k) != 2772)
+	{
+		modify(2772, k);
+		printf("%s\n", k); /*key*/
+	}
+	else if (checkHexSum(k) == 2772)
+	{
+		printf("%s\n", k); /*key*/
+	}
+	else
+	{
+		/*printf("Sorry Key not found!\n");*/
+	}
+	/*printf("Final sum of ASCII Codes: %d \n", checkHexSum(k));*/
 	return (0);
 }
 
-int checkHexSum(char *s)
+/**
+  * checkHexSum - This is a function that calculates the ascii sum of the key
+  * @k: A pointer to the key
+  * Return: an integer sum of the key
+  */
+int checkHexSum(char *k)
 {
-	unsigned int j, sum = 0, temp = 0;
+	unsigned int j;
+	unsigned int sum = 0;
+	unsigned int temp = 0;
 
-	for (j = 0; *(s + j) != '\0'; j++)
+	for (j = 0; *(k + j) != '\0'; j++)
 	{
-		temp = *(s + j);
-		printf("%c", *(s + j));
+		temp = *(k + j);
 		sum = sum + temp;
 	}
-	//printf(" Sum: %d \n", sum);
 	return (sum);
+}
+
+/**
+  * modify - This is a function to modify the key to match the required target
+  * @k: A pointer to the key
+  * @t: the target sum
+  * Return: Always NULL
+  */
+void modify(int t, char *k)
+{
+	int scan = 0;
+	int diff = (checkHexSum(k) - t);
+
+	/*printf("Difference is %d: \n", diff);*/
+	for (; (*(k + scan) != '\0') ;  scan++)
+	{
+	}
+	scan--;
+
+	/*printf("Last character is: %d \n", *(k + scan));*/
+	if (*(k + scan) > diff)
+	{
+		*(k + scan) = *(k + scan) - diff;
+	}
 }

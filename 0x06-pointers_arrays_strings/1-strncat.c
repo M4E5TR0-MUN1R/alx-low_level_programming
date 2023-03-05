33de05ba13da87ca4a2383c0577b3fad5d940fee
M4E5TR0-MUN1R <khamistsanuo@gmail.com>
2023-03-06 02:54:11 +0300

    Task 1: concatenate specified length

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,27 +1,31 @@
-#include "main.h"
 /**
- * _strncat - concatenates two strings,
- * @dest: destination.
- * @src: source.
- * @n: amount of bytes used from src.
- * Return: the pointer to dest.
- */
+  * _strncat - A funtion that concatenates a specified length from source
+  * @dest: A pointer to the destination string
+  * @src: A pointer to the source string
+  * Return: A pointer to the new concatenated string
+  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int count = 0, count2 = 0;
+	int len_dest = 0, len_src = 0, count = 0;
 
-	while (*(dest + count) != '\0')
+	while (*(dest + len_dest) != '\0')
 	{
-		count++;
+		len_dest++;
+	}
+	while (*(src + len_src) != '\0')
+	{
+		len_src++;
 	}
 
-	while (count2 < n)
+	for(count = 0; count < n; count++)
 	{
-		*(dest + count) = *(src + count2);
-		if (*(src + count2) == '\0')
+		if (count == len_src)
+		{
 			break;
-		count++;
-		count2++;
+		}
+		*(dest + len_dest) = *(src + count);
+		len_dest++;
 	}
-	return (dest);
+	*(dest + len_dest) = '\0';
+	return(dest);
 }

71349868a210f5e239abd24523d5ba04f13aa5c2
M4E5TR0-MUN1R <khamistsanuo@gmail.com>
2023-03-11 00:00:46 +0300

    Task 8 Correction: rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,27 +1,25 @@
-#include "main.h"
 /**
- * rot13 - encodes a string using rot13
- * @s: input string.
- * Return: the pointer to dest.
- */
-
+  * rot13 - A function that can be called to encode/decode a string with rot13
+  * @s: A pointer to the string
+  * Return: a pointer to the encoded or decoded string
+  */
 char *rot13(char *s)
 {
-	int count = 0, i;
-	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char alp[52] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[52] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int c, j;
 
-	while (*(s + count) != '\0')
+	for (c = 0; *(s + c) != '\0'; c++)
 	{
-		for (i = 0; i < 52; i++)
+		for (j = 0; j < 52; j++)
 		{
-			if (*(s + count) == alphabet[i])
+			if (*(s + c) == *(alp + j))
 			{
-				*(s + count) = rot13[i];
+				*(s + c) = *(rot + j);
 				break;
 			}
+			continue;
 		}
-		count++;
 	}
 
 	return (s);

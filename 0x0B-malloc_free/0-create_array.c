99c9103d15703138e3bbad55cccb17bba40873f3
M4E5TR0-MUN1R <khamistsanuo@gmail.com>
2023-03-13 21:01:19 +0300

    Task 0: Fixed malloc(0)

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,8 +8,13 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int d;
-	char *ptr = malloc(sizeof(char) * size);
 
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else
+		char *ptr = malloc(sizeof(char) * size);
 	for (d = 0; d <= size ; d++)
 	{
 		*(ptr + d) = c;

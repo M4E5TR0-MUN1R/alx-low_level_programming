95824f7b02bbf97b9d8b5f0127281dfbe5df735a
M4E5TR0-MUN1R <khamistsanuo@gmail.com>
2023-03-13 22:33:54 +0300

    Task 2: fixed empty string problem

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,11 +12,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		*s1 = '\0';
+		*s1 = ' ';
 	}
 	if (s2 == NULL)
 	{
-		*s2 = '\0';
+		*s2 = ' ';
 	}
 	while (*(s1 + l1) != '\0')
 	{
